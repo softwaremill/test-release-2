e450d4074c1d6237dad4a7d2266851e515e7c288
adamw <adam@warski.org>
2019-07-03 08:19:22 +0200

    Attempt to fix release

--- project/Release.scala
+++ project/Release.scala
@@ -18,9 +18,6 @@ object Release {
 
   val settings = Seq(
     isCommitRelease := true,
-    useGpg := false, // use the gpg implementation from the sbt-pgp plugin
-    pgpSecretRing := baseDirectory.value / "secring.asc", // unpacked from secrets.tar.enc
-    pgpPublicRing := baseDirectory.value / "pubring.asc", // unpacked from secrets.tar.enc
     commands += Command.command("commitRelease") { state =>
       "set Release.isCommitRelease := true" ::
         "release" ::
--- release.sbt
+++ release.sbt
@@ -1,1 +1,5 @@
+useGpg := false, // use the gpg implementation from the sbt-pgp plugin
+pgpSecretRing := baseDirectory.value / "secring.asc", // unpacked from secrets.tar.enc
+pgpPublicRing := baseDirectory.value / "pubring.asc", // unpacked from secrets.tar.enc
+
 Release.settings
